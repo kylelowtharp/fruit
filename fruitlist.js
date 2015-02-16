0cd6153d8a3e554e29367333c0ba9e0b6ade5356
kylelowtharp <klv9700@saintjoe.edu>
2015-02-16 12:14:35 -0500

    practice for exam fruit#

---
+++ fruitlist.js
@@ -0,0 +1,5 @@
+'{"fruit": "Apple", "color": "red", "shape": "sphere"}',
+'{"fruit": "banana", "color": "yellow", "shape": "elongated"}',
+'{"fruit": "pear:, "color": "green", "shape": "pear shaped"}',
+'{"fruit": "orange", "color": "orange", "shape": "sphere"}',
+'{"fruit": "grapes", "color": "purple", "shape": "sphere"}',
